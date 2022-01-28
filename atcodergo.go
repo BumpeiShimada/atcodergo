9089c9167e41d8e672c9ad5075867ea882da9dd6
boompay <curryisdrink.sb@gmail.com>
2022-01-28 19:47:57 +0900

    add others for abs

--- atcodergo.go
+++ atcodergo.go
@@ -3,3 +3,24 @@ package atcodergo
 func IsEven(i int) bool {
 	return i%2 == 0
 }
+
+func ReverseString(s string) (result string) {
+	for _, v := range s {
+		result = string(v) + result
+	}
+	return
+}
+
+func Unique(slice []string) []string {
+	m := make(map[string]bool)
+	var uniqueSlice []string
+
+	for _, v := range slice {
+		if _, ok := m[v]; !ok {
+			m[v] = true
+			uniqueSlice = append(uniqueSlice, v)
+		}
+	}
+
+	return uniqueSlice
+}
--- atcodergo_test.go
+++ atcodergo_test.go
@@ -1,6 +1,9 @@
 package atcodergo
 
-import "testing"
+import (
+	"encoding/json"
+	"testing"
+)
 
 func TestIsEven(t *testing.T) {
 	evenNum := 8
@@ -15,5 +18,38 @@ func TestIsEven(t *testing.T) {
 	oddExpected := false
 	if oddActual != oddExpected {
 		t.Errorf("got: %v, want: %v", oddActual, oddExpected)
+	}
+}
+
+func TestReverseString(t *testing.T) {
+	word := "dream wonderland"
+	actual := ReverseString(word)
+	expected := "dnalrednow maerd"
+	if actual != expected {
+		t.Errorf("got: %v, want: %v", actual, expected)
+	}
+}
+
+func TestUnique(t *testing.T) {
+	slice := []string{
+		"alice",
+		"alice",
+		"bob",
+		"dave",
+	}
+	actualSlice := Unique(slice)
+	actualJSON, _ := json.Marshal(actualSlice)
+	actual := string(actualJSON)
+
+	expectedSlice := []string{
+		"alice",
+		"bob",
+		"dave",
+	}
+	expectedJSON, _ := json.Marshal(expectedSlice)
+	expected := string(expectedJSON)
+
+	if string(actual) != expected {
+		t.Errorf("got: %v, want: %v", actual, expected)
 	}
 }
